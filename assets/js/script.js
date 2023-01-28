829d34c1714fa9ed53b83e85d040d9a7d6cfec07
Joey <josephdeshaies@Joeys-MacBook-Pro-2.local>
2023-01-28 16:26:46 -0500

    Updates question objects in array and displays questions asked in console when start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,32 @@
 var timeEl = document.querySelector(".time");
-var mainEl = document.getElementsByTagName(main);
+// var mainEl = document.getElementsByTagName(main);
 var h1El = document.getElementById("page-heading");
 var pEl = document.getElementById("quiz-info")
 var startButton = document.getElementById("start-button")
-var secondsLeft = 10;
+var secondsLeft = 60;
 
 var questionsArray = [
     {
-        question1Asked:"Commonly used data types DO NOT include:",
-        question1Options:["strings","booleans","alerts","numbers"],
-        question1Answer:"alerts"
+        questionAsked:"Commonly used data types DO NOT include:",
+        questionOptions:["strings","booleans","alerts","numbers"],
+        questionAnswer:"alerts"
     },
     {
-        question2Asked:"The condition in an if / else statement is enclosed with __________.",
-        question2Options:["quotes","curly brackets","paranthesis","square brackets"],
-        question2Answer:"paranthesis"
+        questionAsked:"The condition in an if / else statement is enclosed with __________.",
+        questionOptions:["quotes","curly brackets","paranthesis","square brackets"],
+        questionAnswer:"paranthesis"
     },
     {
-        question3Asked:"Arrays in JavaScript can be used to store _________.",
-        question3Options:["numbers and strings","other arrays","booleans","all of the above"],
-        question3Answer:"all of the above"
-    }
+        questionAsked:"Arrays in JavaScript can be used to store _________.",
+        questionOptions:["numbers and strings","other arrays","booleans","all of the above"],
+        questionAnswer:"all of the above"
+    },
     {
-        question4Asked:"String values must be enclosed within __________ when being assigned to variables.",
-        question4Options:["commas","curly brackets","quotes","paranthesis"],
-        question4Answer:"quotes"
+        questionAsked:"String values must be enclosed within __________ when being assigned to variables.",
+        questionOptions:["commas","curly brackets","quotes","paranthesis"],
+        questionAnswer:"quotes"
     }
-]
-
+];
 
 startButton.addEventListener("click", function startTime() {
     // Sets interval in variable
@@ -43,13 +42,19 @@ startButton.addEventListener("click", function startTime() {
       }
   
     }, 1000);
+
+    displayQuestions();
   });
   
-  // Function to create and append colorsplosion image
+  function displayQuestions() {
+    for (var i=0; i<questionsArray.length; i++) {
+        console.log("Question Asked:" + questionsArray[i].questionAsked);
+    }
+  }
+
   function sendMessage() {
     h1El.textContent = "All done!"  
     pEl.textContent = "Your final score is " + secondsLeft.toString() + ".";
+}
 
-  }
-  
-  setTime();
+ 
