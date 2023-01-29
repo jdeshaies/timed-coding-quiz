db482a8dab23620e2ec9470da1128d850d63246c
Joey <josephdeshaies@Joeys-MacBook-Pro-2.local>
2023-01-28 19:01:51 -0500

    Shows final score and message when timer is 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var timeEl = document.querySelector(".time");
 var mainEl = document.querySelector(".main");
 var h1El = document.getElementById("page-heading");
-var pEl = document.getElementById("quiz-info")
-var startButton = document.getElementById("start-button")
+var pEl = document.getElementById("quiz-info");
+var startButton = document.getElementById("start-button");
 var secondsLeft = 5;
+var quizOver = false;
 
 var questionsArray = [
     {
@@ -28,7 +29,7 @@ var questionsArray = [
     }
 ];
 
-startButton.addEventListener("click", function startTime() {
+startButton.addEventListener("click", function startQuiz() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
       secondsLeft--;
@@ -37,6 +38,7 @@ startButton.addEventListener("click", function startTime() {
       if(secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
+        quizOver = true;
         // Calls function to create and append image
         sendMessage();
       }
@@ -44,29 +46,18 @@ startButton.addEventListener("click", function startTime() {
     }, 1000);
 
     displayQuestions();
-  });
-  
-  function displayQuestions() {
-    pEl.setAttribute("hidden", true);
+});
+
+function displayQuestions() {
     startButton.setAttribute("hidden", true);
     var questionNumber = 0;
     var currentQuestion = questionsArray[questionNumber];
-    var questionListEl = document.createElement("ol");
+    h1El.textContent = currentQuestion.questionAsked;
+    pEl.textContent = "";
+};
 
-    while (questionNumber < 1) {
-        h1El.textContent = currentQuestion.questionAsked;
-        for (var i=0; i < currentQuestion.questionOptions.length; i++){
-            var questionOptionEl = document.createElement("li");
-            questionOptionEl.append(currentQuestion.questionOptions[i]);
-            questionListEl.append(questionOptionEl);
-        }
-        mainEl.append(questionListEl);
-        questionNumber++;
-    }
-  }
-
-  function sendMessage() {
-    h1El.textContent = "All done!";  
+function sendMessage() {
+    h1El.textContent = "All done!";
     pEl.textContent = "Your final score is " + secondsLeft.toString() + ".";
 }
 
